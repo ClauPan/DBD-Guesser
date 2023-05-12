c93b73441a6f8f83dae3b74f79b19e6699289cc8
Lokplart <lokplart@gmail.com>
2023-05-12 15:19:33 +0300

    fixing thymeleaf spring security module

--- code/src/main/java/com/example/mvcproducts/security/SecurityConfig.java
+++ code/src/main/java/com/example/mvcproducts/security/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @Configuration
 public class SecurityConfig {
