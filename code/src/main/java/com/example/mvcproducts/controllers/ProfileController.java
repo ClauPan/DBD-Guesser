f457c0cf29ee93a7f2f92808c0bc13dc1ece5143
Lokplart <lokplart@gmail.com>
2023-05-13 11:36:13 +0300

    adding some framework for imagine upload

--- code/src/main/java/com/example/mvcproducts/controllers/ProfileController.java
+++ code/src/main/java/com/example/mvcproducts/controllers/ProfileController.java
@@ -1,24 +1,42 @@
 package com.example.mvcproducts.controllers;
 
 import com.example.mvcproducts.domain.User;
+import com.example.mvcproducts.services.UserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 public class ProfileController {
+    private final String PROFILE_PIC_DIR = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\images\\profiles";
+    private final UserService userService;
+    public ProfileController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/profile/_")
     public String profileRedirect() {
         return "redirect:/profile";
     }
 
-
     @GetMapping("/profile")
     public String showUserProfile(Authentication authentication, Model model) {
         User user = (User) authentication.getPrincipal();
         model.addAttribute("profile", user);
         return "profile";
+    }
+
+    @PostMapping("/profile/pic")
+    public String changeProfilePic(Authentication authentication, @RequestParam("img") file) throws IOException {
+        User user = (User) authentication.getPrincipal();
+        System.out.println(PROFILE_PIC_DIR + "\\" + user.getId().toString() + "\\" + file.getOriginalFilename());
+
+        return "redirect:/profile";
     }
 }
