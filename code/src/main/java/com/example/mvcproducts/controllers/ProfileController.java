24a461301adc9cbea9636f58de1fc77359ab22bf
Lokplart <lokplart@gmail.com>
2023-05-13 12:29:48 +0300

    adding complete file upload functionality for changing profile pic

--- code/src/main/java/com/example/mvcproducts/controllers/ProfileController.java
+++ code/src/main/java/com/example/mvcproducts/controllers/ProfileController.java
@@ -2,15 +2,23 @@ package com.example.mvcproducts.controllers;
 
 import com.example.mvcproducts.domain.User;
 import com.example.mvcproducts.services.UserService;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class ProfileController {
@@ -35,8 +43,16 @@ public class ProfileController {
     @PostMapping("/profile/pic")
     public String changeProfilePic(Authentication authentication, @RequestParam("img")MultipartFile file) throws IOException {
         User user = (User) authentication.getPrincipal();
-        System.out.println(PROFILE_PIC_DIR + "\\" + user.getId().toString() + "\\" + file.getOriginalFilename());
-
+        Path path = Paths.get(PROFILE_PIC_DIR + "\\" + user.getId().toString() + "_" + user.getUsername() + "\\profile.png");
+        Files.write(path, file.getBytes());
         return "redirect:/profile";
+    }
+
+    @GetMapping("/images/profiles/{id}_{username}/profile.png")
+    public ResponseEntity<byte[]> getProfilePic(@PathVariable Long id, @PathVariable String username) throws IOException {
+        Path path = Paths.get(PROFILE_PIC_DIR + "\\" + id.toString() + "_" + username + "\\profile.png");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl(CacheControl.noCache().getHeaderValue());
+        return new ResponseEntity<>(Files.readAllBytes(path), headers, HttpStatus.OK);
     }
 }
