68149db0dd2f0fc9cb551afade3c910667bc838d
Lokplart <lokplart@gmail.com>
2023-05-27 20:47:46 +0300

    added init function

--- code/src/main/java/com/example/mvcproducts/bootstrap/DataLoader.java
+++ code/src/main/java/com/example/mvcproducts/bootstrap/DataLoader.java
@@ -12,9 +12,28 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DataLoader implements CommandLineRunner {
+  private final UserService userService;
 
-  public DataLoader() {}
+  public DataLoader(UserService userService) {
+    this.userService = userService;
+  }
 
   @Override
-  public void run(String... args) {}
+  public void run(String... args) {
+    //init();
+  }
+
+  public void init() {
+    PasswordEncoder bcrypt = new BCryptPasswordEncoder();
+    User user1=new User("user1",bcrypt.encode("user1"), "user1@gmail.com");
+    user1.getRoles().add(Role.ROLE_USER);
+    User user2=new User("user2",bcrypt.encode("user2"), "user2@gmail.com");
+    user2.getRoles().add(Role.ROLE_ADMIN);
+    User user3=new User("user3",bcrypt.encode("user3"), "user3@gmail.com");
+    user3.getRoles().add(Role.ROLE_ADMIN);
+    user3.getRoles().add(Role.ROLE_CREATOR);
+    userService.save(user1);
+    userService.save(user2);
+    userService.save(user3);
+  }
 }
